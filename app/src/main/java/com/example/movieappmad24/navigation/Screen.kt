14463123c1e6b7cbe3c421a0d830e01d439ce634
Janella Gatmaitan <janella.gatmaitan@gmail.com>
2024-03-31 11:39:53 +0200

    mostly done

--- app/src/main/java/com/example/movieappmad24/navigation/Screen.kt
+++ app/src/main/java/com/example/movieappmad24/navigation/Screen.kt
@@ -1,9 +1,15 @@
 package com.example.movieappmad24.navigation
 
-sealed class Screen(val route:String) {
-    data object Home : Screen(route = "homescreen")
-    data object Detail : Screen(route = "detailscreen/{movieId}") {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(val route:String, val icon: ImageVector) {
+    data object Home : Screen(route = "homescreen", Icons.Default.Home)
+    data object Detail : Screen(route = "detailscreen/{movieId}" ,Icons.Default.ArrowBack ) {
         fun createRoute(movieId: String) = "detailscreen/$movieId"
     }
-    data object Watchlist : Screen(route= "watchlist")
+    data object Watchlist : Screen(route = "watchlist", Icons.Default.Star)
 }
--- app/src/main/java/com/example/movieappmad24/ui/components/components.kt
+++ app/src/main/java/com/example/movieappmad24/ui/components/components.kt
@@ -6,15 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
@@ -24,8 +21,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.example.movieappmad24.models.getMovieFromId
 import com.example.movieappmad24.navigation.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -56,18 +53,43 @@ fun AppTopBar(navController: NavController, movieId:String?, title:String) {
 
 @Composable
 fun AppBottomBar(navController: NavController) {
+    val items = listOf(Screen.Home, Screen.Watchlist)
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
     BottomAppBar (
         containerColor = MaterialTheme.colorScheme.primaryContainer,
         contentColor = MaterialTheme.colorScheme.primary,
-    )  {
+    )  {Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically){items.forEach{screen->
+        NavigationBarButton(
+            icon= screen.icon ,
+            selected = currentRoute == screen.route,
+            label = screen.route,
+            onClick = {
+                // Avoid recreating the screen if it's already displayed
+                if (currentRoute != screen.route) {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            }
+        )
+        // {
+        // Navigation logic here
+    }}
 
-            NavigationBarItem(navController)
         }
 
 
 }
+
+
+
 
 @Composable
 fun NavigationBarItem(navController: NavController) {
@@ -76,20 +98,20 @@ fun NavigationBarItem(navController: NavController) {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround
     ) {
-        NavigationBarButton(icon = Icons.Default.Home, label = "Home") {
+        //NavigationBarButton(icon = Icons.Default.Home, label = "Home") {
             // Handle Home action
 
 
         }
-        NavigationBarButton(icon = Icons.Default.Star, label = "Watchlist") {
+       // NavigationBarButton(icon = Icons.Default.Star, label = "Watchlist") {
             // Handle Watchlist action
-            navController.navigate(Screen.Watchlist.route)
-        }
-    }
+        //    navController.navigate(Screen.Watchlist.route)
+       // }
+    //}
 }
 
 @Composable
-fun NavigationBarButton(icon: ImageVector, label: String, onClick: () -> Unit) {
+fun NavigationBarButton(icon: ImageVector, selected: Boolean, label: String, onClick: () -> Unit) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         IconButton(onClick = onClick) {
             Icon(imageVector = icon, contentDescription = label)
